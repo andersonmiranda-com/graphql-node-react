bbd34d2752b69080ea1ca903881b685126a4c8d7
Anderson Miranda <me@andersonmiranda.com>
2021-03-13 13:23:55 +0100

    graphQL types

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const { buildSchema } = require('graphql');
 const app = express();
 app.use(express.json());
 
+const events = [];
+
 app.get('/', (req, res, next) => {
     res.send('Hello World');
 });
@@ -12,13 +14,28 @@ app.get('/', (req, res, next) => {
 app.use('/graphql',
     graphqlHTTP({
         schema: buildSchema(`
+
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
         
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
 
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput): Event
         }
 
         schema {
@@ -29,11 +46,18 @@ app.use('/graphql',
     `),
         rootValue: {
             events: () => {
-                return ['NodeJs training', 'CTO Meeting', 'Cooking a paella']
+                return events;
             },
-            createEevent: (args) => {
-                const eventName = args.name;
-                return eventName;
+            createEvent: (args) => {
+                const event = {
+                    _id: Math.random().toString(),
+                    title: args.eventInput.title,
+                    description: args.eventInput.description,
+                    price: +args.eventInput.price,
+                    date: args.eventInput.date,
+                };
+                events.push(event);
+                return event;
             }
         },
         graphiql: true
